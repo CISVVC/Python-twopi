078456153f742d1726b2a92b013ac3977612675f
Paul Tonning <paultonning@edoras.local>
2017-06-08 13:46:29 -0700

    refactor

--- mainwindow.py
+++ mainwindow.py
@@ -19,31 +19,6 @@ from PyQt5.QtWidgets import (
 )
 
 
-def import_module(name, package=None):
-    """An approximate implementation of import."""
-    absolute_name = importlib.util.resolve_name(name, package)
-    try:
-        return sys.modules[absolute_name]
-    except KeyError:
-        pass
-
-    path = None
-    if '.' in absolute_name:
-        parent_name, _, child_name = absolute_name.rpartition('.')
-        parent_module = import_module(parent_name)
-        path = parent_module.spec.submodule_search_locations
-    for finder in sys.meta_path:
-        spec = finder.find_spec(absolute_name, path)
-        if spec is not None:
-            break
-    else:
-        raise ImportError(f'No module named {absolute_name!r}')
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    sys.modules[absolute_name] = module
-    if path is not None:
-        setattr(parent_module, child_name, module)
-    return module
 from view import View
 
 class Window(QMainWindow):
